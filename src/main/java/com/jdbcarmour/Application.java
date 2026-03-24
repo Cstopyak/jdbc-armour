a88fc2e5e76c3e3aebd1a4285b9aab9faec4ff44
cstopyak <cstopyak22@gmail.com>
2026-03-23 20:36:20 -0500

    addding rest of repo

---
+++ src/main/java/com/jdbcarmour/Application.java
@@ -0,0 +1,10 @@
+package com.jdbcarmour;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
